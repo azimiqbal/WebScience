82482bc1979e81854ef137917b330524f7e1b26f
azimiqbal <47232822+azimiqbal@users.noreply.github.com>
2020-04-13 23:49:10 +0200

    Create MyBlog.php

---
+++ MyBlog.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
+</style>
+<body class="w3-light-grey">
+
+
+<div class="w3-content" style="max-width:1200px""max-height: 50%">
+
+
+<header class="w3-container w3-center w3-padding-32"> 
+  <a class="navbar-brand" href="dashboard.php">Gå tilbake</a>
+  <h1><b>Mario's BLOGG</b></h1>
+  <p>Velkommen til min blogg</p>
+</header>
+
+
+<div class="w3-row">
+
+<!-- Blog entries -->
+<div class="w3-col l8 s12">
+
+  <div class="w3-card-4 w3-margin w3-white">
+    <img src="images/parasite.jpg" alt="Nature" style="width:100%" height="550">
+    <div class="w3-container">
+      <h3><b>Parasite</b></h3>
+      <h5><span class="w3-opacity">April 13, 2020</span></h5>
+    </div>
+
+    <div class="w3-container">
+      <p>Parasite vant beste film i 2019, og nå vet jeg hvorfor...</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+          <p><button class="w3-button w3-padding-large w3-white w3-border"><b>LES MER »</b></button></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr>
+
+
+  <div class="w3-card-4 w3-margin w3-white">
+  <img src="images/don.jpg" alt="Norway" style="width:100%" height="550">
+    <div class="w3-container">
+      <h3><b>Gudfaren</b></h3>
+      <h5><span class="w3-opacity">April 13, 2020</span></h5>
+    </div>
+
+    <div class="w3-container">
+      <p>Jeg så endelig på Gudfaren!</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+          <p><button class="w3-button w3-padding-large w3-white w3-border"><b>LES MER »</b></button></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+<div class="w3-col l4">
+
+  <div class="w3-card w3-margin w3-margin-top">
+    <div class="w3-container w3-white">
+      <h4><b>Om meg</b></h4>
+      <p>Hei! Jeg heter Mario! Jeg liker å se på filmer og snakke om dem.</p>
+    </div>
+  </div><hr>
+  
+
+  <div class="w3-card w3-margin">
+    <div class="w3-container w3-padding">
+      <h4>Populære blogginnlegg</h4>
+    </div>
+    <ul class="w3-ul w3-hoverable w3-white">
+      <li class="w3-padding-16">
+        <span class="w3-large">1917</span><br>
+        <span>Må sees!</span>
+      </li>
+      <li class="w3-padding-16">
+        <span class="w3-large">Joker</span><br>
+        <span>En annerledes opplevelse</span>
+      </li> 
+      <li class="w3-padding-16">
+        <span class="w3-large">Knives out</span><br>
+        <span>Skuffende</span>
+      </li>   
+      <li class="w3-padding-16 w3-hide-medium w3-hide-small">
+        <span class="w3-large">Dunkirk</span><br>
+        <span>Fantastisk kino opplevelse</span>
+      </li>  
+    </ul>
+  </div>
+  <hr> 
+  </div>
+  
+
+</div>
+
+
+</div><br>
+
+
+</div>
+
+<footer class="w3-container w3-dark-grey w3-padding-32 w3-margin-top">
+  <button class="w3-button w3-black w3-disabled w3-padding-large w3-margin-bottom">Forrige side</button>
+  <button class="w3-button w3-black w3-padding-large w3-margin-bottom">Neste side »</button>
+</footer>
+
+</body>
+</html>
