75d2d26a9148cae6f9f0f85a5637c82e2331f9d4
azimiqbal <47232822+azimiqbal@users.noreply.github.com>
2020-04-13 23:45:29 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<body class="d-flex flex-column">
+  <div id="page-content">
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col-md-7">
+          <h1 class="font-weight-light mt-4 text-white">Sticky Footer using Flexbox</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small>Copyright &copy; Røa osloskolen 2020</small>
+    </div>
+  </footer>
+</body>
