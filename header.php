383e56e9602ffdcb143ce14f30eba26583406240
azimiqbal <47232822+azimiqbal@users.noreply.github.com>
2020-04-13 23:45:10 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,40 @@
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    
+  <link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  </head>
+
+
+
+
+<body>
+  
+	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <a class="navbar-brand" href="index.php">Røa smart-skole</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item"></li>
+        <a class="nav-link" href="#">For elever og foresatte</a>
+      <li class="nav-item"></li>
+        <a class="nav-link" href="#">Vår teknologi</a>
+      <li class="nav-item"></li>
+        <a class="nav-link" href="#">Livet på skolen</a>
+      <li class="nav-item"></li>
+        <a class="nav-link" href="#">Kontaks oss</a>
+      </ul>
+  <ul class="navbar-nav m-auto">
+    <li class="nav-item fluid"></li>
+      <a class="navbar-brand" href="index2.php"><img src="images/login.png" width="27px"></a>
+      <a class="nav-link" href="index2.php">Logg inn <span class="sr-only">(current)</span></a>
+    	</ul>
+  	</div>
+   </nav>
